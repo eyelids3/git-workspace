2cb15d745f0b82f955eca4a32246055042be5ba8
eyelids <eyelids_bo@163.com>
2022-12-26 18:20:59 +0800

    002 commit

--- src/main/java/com/eyelids/GitTest.java
+++ src/main/java/com/eyelids/GitTest.java
@@ -3,5 +3,6 @@ package com.eyelids;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("你好！Git");
+        System.out.println("Git 002 commit");
     }
 }
