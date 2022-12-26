6cf3199acc19410b687f79b38f3ac08c17a5b3d1
eyelids <eyelids_bo@163.com>
2022-12-26 18:26:03 +0800

    001 hot-fix merge

--- src/main/java/com/eyelids/GitTest.java
+++ src/main/java/com/eyelids/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("你好！Git");
         System.out.println("Git 002 commit");
+        System.out.println("hot-fix merge");
     }
 }
